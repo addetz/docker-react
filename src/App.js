2f7b398d4a1347dc1939c4503549736c328f70d2
addetz <adelinasimion@gmail.com>
2020-08-29 17:34:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Adelina wuz here m8. 
+          Ehhh, dem's the automatic refreshes.
+          And again! 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
